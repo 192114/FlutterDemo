2f24b04f65a7a955816fc1f410a61ed52594bc46
Shadow Sun <shadowsun@shadow.local>
2019-07-12 23:26:46 +0800

    路由动画效果学习

--- demo03/lib/custom_route.dart
+++ demo03/lib/custom_route.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CustomRoute extends PageRouteBuilder {
   final Widget widget;
@@ -17,9 +18,35 @@ class CustomRoute extends PageRouteBuilder {
                 Animation<double> animation1,
                 Animation<double> animation2,
                 Widget child) {
-              return FadeTransition(
-                opacity: Tween(begin: 0.0, end: 1.0).animate(
-                    CurvedAnimation(curve: Curves.easeIn, parent: animation1)),
+              // 渐隐渐现
+              // return FadeTransition(
+              //   opacity: Tween(begin: 0.0, end: 1.0).animate(
+              //       CurvedAnimation(curve: Curves.easeIn, parent: animation1)),
+              //   child: child,
+              // );
+              // 缩放
+              // return ScaleTransition(
+              //   scale: Tween(begin: 0.0, end: 1.0).animate(
+              //     CurvedAnimation(curve: Curves.easeIn, parent: animation1),
+              //   ),
+              //   child: child, // 用作动画效果叠加
+              // );
+              // 旋转加缩放
+              // return RotationTransition(
+              //   turns: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+              //       curve: Curves.fastOutSlowIn, parent: animation1)),
+              //   child: ScaleTransition(
+              //     scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+              //         curve: Curves.fastOutSlowIn, parent: animation1)),
+              //     child: child,
+              //   ),
+              // );
+
+              return SlideTransition(
+                position: Tween<Offset>(
+                        begin: Offset(-1.0, 0.0), end: Offset(0.0, 0.0))
+                    .animate(CurvedAnimation(
+                        curve: Curves.fastOutSlowIn, parent: animation1)),
                 child: child,
               );
             });
