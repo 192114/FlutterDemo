7f8334095c0bf8b81a2199c4096d0d171122d144
Shadow Sun <shadowsun@shadow.local>
2019-07-15 22:27:27 +0800

    tab 切换保持页面状态

---
+++ demo04/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:demo04/keepAliveWidget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'demo04',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: KeepAliveWidget(),
+    );
+  }
+}
+
+class KeepAliveWidget extends StatefulWidget {
+  @override
+  _KeepAliveWidgetState createState() => _KeepAliveWidgetState();
+}
+
+class _KeepAliveWidgetState extends State<KeepAliveWidget> with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('keepAliveDemo'),
+        bottom: TabBar(
+          controller: _controller,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.dashboard),),
+            Tab(icon: Icon(Icons.satellite),),
+            Tab(icon: Icon(Icons.directions_bike),)
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: <Widget>[
+          MyHomePage(),
+          MyHomePage(),
+          MyHomePage(),
+        ],
+      ),
+    );
+  }
+}
